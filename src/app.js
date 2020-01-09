8e91ed5ddd805dc772103ba2e64589b40d12cb4e
Brian <briansmith2@quickenloans.com>
2020-01-09 14:52:35 -0500

    Fixed Index route

--- src/app.js
+++ src/app.js
@@ -5,14 +5,13 @@ const express = require('express');
 
 const app = express();
 
-var router = express.Router();
+//var router = express.Router();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-/* GET home page. */
-router.get('/', (req, res) => res.render('index', { title: 'Index' }));
+app.get('/', (req, res) => res.render('index', { title: 'Index' }));
 
 app.listen(3000, () => console.log(`PS Project Running on port 3000!`));
